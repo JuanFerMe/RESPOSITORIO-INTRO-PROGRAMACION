34c55091c4011c39a353505141b8a52de558cc20
JuanFerMe <113810640+JuanFerMe@users.noreply.github.com>
2022-11-13 15:03:18 -0600

    Add files via upload

---
+++ CLASE TEORÍA/SEMANA 6-10/Ejercicio rectangulo, Juan Fernando Mérda.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime;
+
+Console.WriteLine("Perimetro y Area de un Rectangulo");
+
+int ancho, largo;
+int area, perimetro;
+
+Console.WriteLine("Ingrese un valor para el ancho del Rectangulo: ");
+ancho = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese un valor para el largo del Rectangulo: ");
+largo = Convert.ToInt32(Console.ReadLine());
+
+area = ancho * largo;
+perimetro = ancho + largo + ancho + largo;
+
+Console.WriteLine("El perimetro es: " + perimetro);
+Console.WriteLine("El area es: " + area);
+
+
+
+
+
+
+
+
